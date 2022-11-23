efb3c77fde171834055bb7246f5d895fc02deb76
joker69216 <hamaraad883@gmail.com>
2022-11-23 13:30:47 +0300

    add search through

--- src/Componenets/GameList.jsx
+++ src/Componenets/GameList.jsx
@@ -1,17 +1,30 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { useState, useEffect } from 'react';
 
 export default function GameList() {
-  const done = useSelector(state => state.data.done);
-  const list = useSelector(state => state.data.items);
+  const done = useSelector((state) => state.data.done);
+  const list = useSelector((state) => state.data.items);
+  const [filteredArr, setfilter] = useState([]);
+
+  useEffect(() => {
+    setfilter([...list]);
+  }, [list]);
+
+  const searchHandler = (e) => {
+    const search = e.target.value;
+    const copy = list.filter(item => item.title.includes(search));
+    setfilter(copy);
+  };
 
   return (
     <div>
+      <input type="text" onChange={searchHandler} />
       {done ? (
-        list.map((item) => {
+        filteredArr.map((item) => {
           return <div key={item.id}> {item.title}</div>;
         })
       ) : (
-          <div>Loading...</div>
+        <div>Loading...</div>
       )}
     </div>
   );
