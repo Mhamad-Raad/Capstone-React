86dc24eab16f711845b7e0f21f918cf2fdaf544d
joker69216 <hamaraad883@gmail.com>
2022-11-24 11:47:57 +0300

    add design to the details page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,27 @@
-
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
+import css from "./Details.module.css";
 
 export default function Details() {
   const location = useLocation();
-  
+
   const game = location.state;
 
   return (
-    <div>{game.title}</div>
-  )
+    <div className={css.details_bg}>
+      <img src={game.image} className={css.img} />
+      <div className={css.title_bg}>
+        <h2 className={css.title}>{game.title}</h2>
+      </div>
+      <div className={css.details}>
+        <div className={css.sub_section}>
+          <h3 className={css.sub_title}>Rating</h3>
+          <p className={css.sub_text}>{game.rating}/5</p>
+        </div>
+        <div className={css.sub_section}>
+          <h3 className={css.sub_title}>Playtime</h3>
+          <p className={css.sub_text}>{game.playtime} (hrs)</p>
+        </div>
+      </div>
+    </div>
+  );
 }
